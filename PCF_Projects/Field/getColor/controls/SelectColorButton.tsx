ceb6b374bd3bca1783476edf0ddacf165828cca8
Daniel Montiel <daniel.montiel@nagarro.com>
2020-05-27 14:31:00 -0500

    🚀 PCF First Release - ColorPicker

---
+++ PCF_Projects/Field/getColor/controls/SelectColorButton.tsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import { IInputs } from "../generated/ManifestTypes";
+import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
+import { Panel } from 'office-ui-fabric-react/lib/Panel';
+import { useConstCallback } from '@uifabric/react-hooks';
+import { mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
+import { ColorPicker, IColor, Stack, IStackItemStyles, DefaultPalette } from 'office-ui-fabric-react/lib/index';
+
+export interface Iprops {
+    pcfContext: ComponentFramework.Context<IInputs>,
+    color: string,
+    updateColor: (_color: string) => void
+}
+
+export const SelectColorButton: React.FC<Iprops> = (props) => {
+
+    const [color, setcolor] = React.useState(props.color);
+    const [isEditable, setisEditable] = React.useState(props.pcfContext.parameters.CodigoColor.security?.editable);
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const openPanel = useConstCallback(() => setIsOpen(true));
+    const dismissPanel = useConstCallback(() => setIsOpen(false));
+    const _updateColor = useConstCallback((ev: any, colorObj: IColor) => setcolor(colorObj.str));
+    const onRenderFooterContent = React.useCallback(() => (
+        <div>
+            <PrimaryButton
+                onClick={() => {
+                    props.updateColor(color);
+                    dismissPanel();
+                }}
+                styles={buttonStyles}>
+                Seleccionar Color
+                    </PrimaryButton>
+            <DefaultButton
+                onClick={() => {
+                    setcolor(props.color);
+                    dismissPanel();
+                }}>
+                Cancelar
+                    </DefaultButton>
+        </div>
+        ), [color]);
+
+    const buttonStyles = { root: { marginRight: 8 } };
+    const classNames = mergeStyleSets({
+        wrapper: {
+            display: 'flex'
+        },
+        preview: {
+            "margin": "auto",
+            "width": "150px",
+            "background-color": color,
+            "height": "150px"
+        },
+        previewsmall: {
+            "margin": "auto",
+            "width": "2.1em",
+            "background-color": color,
+            "height": "2.1em"
+        }
+    });
+    const stackItemStyles: IStackItemStyles = {
+        root: {
+            color: DefaultPalette.white,
+            padding: 5,
+        },
+    };
+
+
+    return (
+        <div>
+            <Stack horizontal disableShrink horizontalAlign="center">
+                <Stack.Item align="center" >
+                    <span>{color.toUpperCase()}</span>
+                </Stack.Item>
+                <Stack.Item align="center" styles={stackItemStyles}>
+                    <div className={classNames.previewsmall} />
+                </Stack.Item>
+                <Stack.Item align="center" styles={stackItemStyles}>
+                    <PrimaryButton
+                        text="Seleccionar Color"
+                        onClick={openPanel}
+                        allowDisabledFocus
+                        disabled={!isEditable} />
+                </Stack.Item>
+            </Stack>
+            <div>
+                <Panel
+                    isOpen={isOpen}
+                    onDismiss={dismissPanel}
+                    headerText="Seleccionar Color"
+                    closeButtonAriaLabel="Cerrar"
+                    onRenderFooterContent={onRenderFooterContent}
+                    isFooterAtBottom={true}
+                    type={1}
+                >
+                    <div className={classNames.wrapper}>
+                        <ColorPicker
+                            color={color}
+                            alphaType="none"
+                            onChange={_updateColor}
+                        />
+                    </div>
+                    <div className={classNames.preview}/>
+                </Panel>
+            </div>
+        </div>
+    );
+};
---
+++ PCF_Projects/Field/getColor/index.ts
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import { SelectColorButton, Iprops } from "./controls/SelectColorButton"
+import { getColorFromString, IColor } from 'office-ui-fabric-react/lib/index';
+
+export class getColor implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+	private _color: 				string;
+	private _notifyOutputChanged: 	() => void;
+	private _context: 				ComponentFramework.Context<IInputs>;
+	private _container: 			HTMLDivElement;	
+	private _props: 				Iprops;
+
+	private _updateColor(_color: string) {
+		this._color = _color.toUpperCase();
+		this._notifyOutputChanged();
+	}
+	
+	constructor() {
+
+	}
+
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
+		this._context = context;
+		this._container = container;
+		this._notifyOutputChanged = notifyOutputChanged;
+		this._color = "#FFFFFF";
+	}
+
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
+		this._context = context;
+		if (context.parameters.CodigoColor.raw! != "val" || "") {
+			if (getColorFromString(context.parameters.CodigoColor.raw!)) {
+				this._color = context.parameters.CodigoColor.raw!;
+			}
+		}
+		this._props = {
+			color: this._color,
+			pcfContext:this._context,
+			updateColor: this._updateColor.bind(this)
+		}
+		ReactDOM.render(
+			React.createElement(SelectColorButton, this._props),
+			this._container,
+		)
+	}
+
+	public getOutputs(): IOutputs {
+		return {
+			CodigoColor: this._color
+		}
+	}
+
+	public destroy(): void {
+		ReactDOM.unmountComponentAtNode(this._container)
+	}
+
+}
